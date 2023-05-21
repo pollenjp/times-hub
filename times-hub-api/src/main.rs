73abef589a92b6758a81388a8f24920fb115d65e
pollenjp <polleninjp@gmail.com>
2023-05-21 17:02:04 +0900

    Add test root page

--- times-hub-api/src/main.rs
+++ times-hub-api/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+use axum::Router;
+use dotenv::dotenv;
+use std::env;
+use std::net::SocketAddr;
+
+fn init_logging() {
+    let log_level = env::var("RUST_LOG").unwrap_or("info".to_string());
+    env::set_var("RUST_LOG", log_level);
+    tracing_subscriber::fmt::init();
+}
+
+#[tokio::main]
+async fn main() {
+    init_logging();
+
+    dotenv().ok();
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("Listening on {}", addr);
+
+    let app = create_app();
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+fn create_app() -> Router {
+    Router::new().route("/", get(root))
+    // .route("/workspaces", .get(all_workspaces::<T>))
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
 }
