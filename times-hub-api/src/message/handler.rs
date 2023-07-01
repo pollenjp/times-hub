8972d8a7ac049ffdd023554667ed5e9a53fde67c
pollenjp <polleninjp@gmail.com>
2023-07-01 22:04:51 +0900

    Add discord sender

--- times-hub-api/src/message/handler.rs
+++ times-hub-api/src/message/handler.rs
@@ -19,58 +19,67 @@ pub struct MessagePayload {
 }
 
 // TODO: ビジネスロジックの分離
-pub async fn send_message<T>(
+pub fn send_message<T>(
     Extension(repo): Extension<Arc<T>>,
     ValidatedJson(payload): ValidatedJson<MessagePayload>,
-) -> StatusCode
+) -> impl std::future::Future<Output = StatusCode> + Send + 'static
 where
     T: WorkspaceRepository,
 {
-    // db から一覧取得
-    let r = repo.all().await.map_err(repository_error_to_status_code);
-    let ws_vec;
-    match r {
-        Ok(w) => {
-            ws_vec = w;
+    async move {
+        // db から一覧取得
+        let r = repo.all().await.map_err(repository_error_to_status_code);
+        let ws_vec;
+        match r {
+            Ok(w) => {
+                ws_vec = w;
+            }
+            Err(e) => return e,
         }
-        Err(e) => return e,
-    }
 
-    // 一覧から targets に含まれるものを抽出
-    let mut targets = HashSet::new();
-    for id in payload.targets {
-        targets.insert(id);
-    }
+        // 一覧から targets に含まれるものを抽出
+        let mut targets = HashSet::new();
+        for id in payload.targets {
+            targets.insert(id);
+        }
 
-    let ws_vec = ws_vec
-        .into_iter()
-        .filter(|ws| targets.contains(&ws.id))
-        .collect::<Vec<_>>();
+        let ws_vec: Vec<entity::Workspace> = ws_vec
+            .into_iter()
+            .filter(|ws| targets.contains(&ws.id))
+            .collect::<Vec<_>>();
 
-    let mut err_msgs: Vec<String> = vec![];
+        let mut err_msgs: Vec<String> = vec![];
 
-    for ws in ws_vec {
-        tracing::info!("send to webhook");
+        for ws in ws_vec {
+            tracing::info!("send to webhook");
 
-        let sender = get_sender(ws.ws_type, ws.webhook_url.as_str(), payload.text.as_str());
-        match sender {
-            Ok(s) => match s.send().await {
-                Ok(_) => {}
+            let sender: Arc<Box<dyn Sender>>;
+            match get_sender(ws.ws_type, ws.webhook_url.as_str(), payload.text.as_str()) {
+                Ok(s) => {
+                    sender = Arc::new(s);
+                }
+                Err(e) => {
+                    err_msgs.push(e.to_string());
+                    continue;
+                }
+            }
+
+            match sender.send().await {
+                Ok(_) => {
+                    tracing::info!("success");
+                }
                 Err(e) => {
                     err_msgs.push(e.to_string());
                 }
-            },
-            Err(e) => {
-                err_msgs.push(e.to_string());
             }
         }
-    }
 
-    // TODO: エラーの詳細を返す
-    if err_msgs.len() > 0 {
-        tracing::error!("error: {:?}", err_msgs);
-        return StatusCode::INTERNAL_SERVER_ERROR;
-    }
+        // TODO: エラーの詳細を返す
+        if err_msgs.len() > 0 {
+            tracing::error!("error: {:?}", err_msgs);
+            return StatusCode::INTERNAL_SERVER_ERROR;
+        }
 
-    StatusCode::OK
+        StatusCode::OK
+    }
 }
--- times-hub-api/src/message/service.rs
+++ times-hub-api/src/message/service.rs
@@ -4,6 +4,7 @@ use ::anyhow::Result;
 use ::axum::async_trait;
 use ::serde::Deserialize;
 use ::serde::Serialize;
+use ::std::boxed::Box;
 use ::thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -12,28 +13,28 @@ pub enum MessageError {
     Unexpected(String),
 }
 
+pub fn get_sender(
+    ws_type: WorkspaceType,
+    webhook_url: &str,
+    text: &str,
+) -> Result<Box<dyn Sender>, MessageError> {
+    match ws_type {
+        WorkspaceType::Slack => {
+            return Ok(Box::new(SlackSender::new(webhook_url, text)));
+        }
+        WorkspaceType::Discord => {
+            return Ok(Box::new(DiscordSender::new(webhook_url, text)));
+        } // _ => Err(MessageError::Unexpected(format!("Unknown workspace type: {}", ws_type)).into()),
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct SlackMessagePayload {
     pub text: String,
 }
 
-pub fn get_sender(
-    ws_type: WorkspaceType,
-    webhook_url: &str,
-    text: &str,
-) -> Result<impl Sender, MessageError> {
-    match ws_type {
-        WorkspaceType::Slack => Ok(SlackSender {
-            webhook_url: webhook_url.to_string(),
-            text: text.to_string(),
-        }),
-        // TODO: discord
-        _ => Err(MessageError::Unexpected(format!("Unknown workspace type: {}", ws_type)).into()),
-    }
-}
-
 #[async_trait]
-pub trait Sender {
+pub trait Sender: Send + Sync {
     async fn send(&self) -> Result<()>;
 }
 
@@ -43,12 +44,72 @@ pub struct SlackSender {
     text: String,
 }
 
+impl SlackSender {
+    pub fn new(webhook_url: &str, text: &str) -> Self {
+        Self {
+            webhook_url: webhook_url.to_string(),
+            text: text.to_string(),
+        }
+    }
+}
+
 #[async_trait]
 impl Sender for SlackSender {
     async fn send(&self) -> Result<()> {
         let payload = SlackMessagePayload {
             text: self.text.clone(),
         };
+
+        reqwest::Client::new()
+            .post(&self.webhook_url)
+            .json(&payload)
+            .send()
+            .await
+            .map_err(|e| {
+                MessageError::Unexpected(format!("Failed to send message to webhook: {}", e))
+            })?;
+        Ok(())
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct DiscordMessagePayload {
+    pub content: String,
+    pub username: String, // "pollenJP Webhook"
+    pub avatar_url: String,
+}
+
+impl DiscordMessagePayload {
+    pub fn new(text: &str) -> Self {
+        Self {
+            content: text.to_string(),
+            // TODO: fix hard coded
+            username: "pollenJP Webhook".to_string(),
+            // TODO: fix hard coded
+            avatar_url: "https://i.gyazo.com/e5b2988a9f6f05cafa4ec384fae1a3dd.png".to_string(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct DiscordSender {
+    webhook_url: String,
+    text: String,
+}
+
+impl DiscordSender {
+    pub fn new(webhook_url: &str, text: &str) -> Self {
+        Self {
+            webhook_url: webhook_url.to_string(),
+            text: text.to_string(),
+        }
+    }
+}
+
+#[async_trait]
+impl Sender for DiscordSender {
+    async fn send(&self) -> Result<()> {
+        let payload = DiscordMessagePayload::new(&self.text);
 
         reqwest::Client::new()
             .post(&self.webhook_url)
